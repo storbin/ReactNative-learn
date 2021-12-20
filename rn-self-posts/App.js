f10dca875d5af9c17e19d4d697842127d7b762d7
storbin <s.torbin@sharp-dev.net>
2021-12-20 14:55:52 +0300

    learn routing

--- rn-self-posts/App.js
+++ rn-self-posts/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import { bootstrap } from './src/bootstrap'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [isReady, setIsReady] = useState(false)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return isReady ? (
+		<View>
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	) : (
+		<AppLoading startAsync={bootstrap} onFinish={() => setIsReady(true)} onError={(err) => console.log(err)} />
+	)
+}
